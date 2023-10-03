d179de5450a2ab68c515604f883265ffcd7b5b68
Yes sin <34749@ma-web.nl>
2023-10-03 13:59:27 +0200

    musiclist

---
+++ 02 - docker - php-mysql/public/index.php
@@ -0,0 +1,9 @@
+<html>
+
+<?php
+    for ($x = 0; $x <= 10; $x++) {
+        echo "nummer 1:Travis scott, song Goosbumps <br>";
+      }
+?>
+
+</html>
---
+++ 02 - docker - php-mysql/source/data.php
@@ -0,0 +1,26 @@
+<?php
+
+$library = [
+    [
+        'artist'  => 'leuk',
+        'title'   => 'mooi',
+        'genre'   => 'goed',
+        'year'    => '2222',
+    ];
+    $item_2 = [
+        'artist'  => '',
+        'title'   => '',
+        'genre'   => '',
+        'year'    => '',
+    ];
+    $item_3 = [
+        'artist'  => '',
+        'title'   => '',
+        'genre'   => '',
+        'year'    => '', 
+    ];
+];
+
+echo $library[0]['title'];
+
+?>
